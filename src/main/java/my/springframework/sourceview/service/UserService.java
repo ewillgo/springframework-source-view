f64d50d8a5ce6d7c95e6830a7a5c00e984830c78
will <ewill.leung@foxmail.com>
2020-05-24 12:20:43 +0800

    init

--- src/main/java/my/springframework/sourceview/service/UserService.java
+++ src/main/java/my/springframework/sourceview/service/UserService.java
@@ -1,9 +1,13 @@
 package my.springframework.sourceview.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private OrderService orderService;
 
     public UserService() {
         System.out.println("UserService constructor created");
